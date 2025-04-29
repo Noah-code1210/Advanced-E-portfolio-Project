dbd4700a14c1294ed00869132cef75cc3a0bf1fe
Noah <noahjames1035@gmail.com>
2025-04-28 20:10:44 -0400

    JavaScript Start

---
+++ index.js
@@ -0,0 +1,14 @@
+// template_ujadqti
+// service_tgdn6n9
+// MHJ3dcy-OK4NGMiSR
+
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init({
+        publicKey: "MHJ3dcy-OK4NGMiSR",
+      });
+   })();
+</script>
