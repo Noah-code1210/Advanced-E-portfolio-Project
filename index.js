8aa0e81805dd56facd23bd1ef86b229a71019a45
Noah <noahjames1035@gmail.com>
2025-04-29 17:53:15 -0400

    Dark Mode Responsiveness Finished

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -23,6 +37,12 @@ function contact(event) {
     });
 }
 
+
 function toggleModal() {
-    // toggle modal
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
